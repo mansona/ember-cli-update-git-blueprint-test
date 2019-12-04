6b342e9c6124a2bcd5d1a08b6d256baafa9b2d36
Kelly Selden <kellyselden@gmail.com>
2019-12-04 20:15:25 +0000

    remove ember-cli-update.json

--- index.js
+++ index.js
@@ -3,10 +3,8 @@
 module.exports = {
   name: require('./package').name,
 
-  locals(options) {
+  locals() {
     return {
-      name: options.entity.name,
-      blueprintLocation: require('./package').repository.url,
       blueprintVersion: require('./package').version
     };
   }
